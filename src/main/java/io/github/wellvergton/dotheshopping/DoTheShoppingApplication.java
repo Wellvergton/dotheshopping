1473868755306b3e58e653120e5f41d76e083406
Wellvergton Marques <wmc.jp@outlook.com>
2021-10-01 15:18:27 -0300

    First commit, Spring initial setup

---
+++ src/main/java/io/github/wellvergton/dotheshopping/DoTheShoppingApplication.java
@@ -0,0 +1,13 @@
+package io.github.wellvergton.dotheshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoTheShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoTheShoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/wellvergton/dotheshopping/DoTheShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.wellvergton.dotheshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoTheShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
